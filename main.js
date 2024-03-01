7e7a52d4b485b2223c464eac51ea58c359e4ec55
M.heloisaNICOLAU <110687840+heloisa-MN@users.noreply.github.com>
2024-03-01 11:34:19 -0300

    xx

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-//* contador aqui
-const contadores = document.querySelectorAll(".contador"); 
-const tempoObjetivo1 = new Date("2024-10-05"); 
 
-contadores[0].textContent = tempo0bjetivo1; 
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
