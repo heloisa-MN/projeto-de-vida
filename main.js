4def9ef33ada33c0c7330f761000ad0d7da0a93d
M.heloisaNICOLAU <110687840+heloisa-MN@users.noreply.github.com>
2024-03-01 10:50:22 -0300

    s

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botões);
-
 for(let i=0;i <botoes.length;i++){
-    console.log(i);
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
 }
