5e3b95a6acd402cf17fc94cd6f1934f54dd32113
M.heloisaNICOLAU <110687840+heloisa-MN@users.noreply.github.com>
2024-04-05 11:02:50 -0300

    t

--- main.js
+++ main.js
@@ -29,6 +29,5 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    return dias + " dias" + horas + " horas " ;
+    return dias + " dias" + horas + " horas " + minutos + " minutos " + segundos + "segundos";
   }
-  
